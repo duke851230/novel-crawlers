ef6892152ade2e5a144d3d3371b407af175fd24b
Duke <as124122323@gmail.com>
2025-10-23 14:59:53 +0800

    feat: add retry system

--- helpers.py
+++ helpers.py
@@ -1,5 +1,30 @@
+import time
+import functools
 import requests
 from requests.models import Response
+from requests.exceptions import RequestException
+
+
+def retry(max_retries=3, delay=5):
+    """
+    A decorator for retrying a function if a RequestException occurs.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(max_retries):
+                try:
+                    return func(*args, **kwargs)
+                except RequestException as e:
+                    print(f"Attempt {attempt + 1} failed: {e}")
+                    if attempt + 1 < max_retries:
+                        print(f"Retrying in {delay} seconds...")
+                        time.sleep(delay)
+                    else:
+                        print("Max retries reached. Failing.")
+                        raise
+        return wrapper
+    return decorator
 
 
 def create_file(file_path: str) -> None:
@@ -14,10 +39,12 @@ def add_content_to_file(file_path: str, content: str) -> None:
     with open(file_path, "a", encoding="utf-8") as f:
         f.write(f"{content}\n\n\n")
 
+@retry()
 def get_html_content(url: str) -> str:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     response: Response = requests.get(url, headers=headers)
+    response.raise_for_status()
     response.encoding = "utf-8"
     return response.text
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from crawlers import Hjwzw
 
 if __name__ == "__main__":
     # 全本爬取
-    # Hjwzw(
-    #     novel_id=37018, 
-    #     novel_name="瘋狂升級系統"
-    # ).main()
+    Hjwzw(
+        novel_id=40949, 
+        novel_name="我師兄實在太穩健了"
+    ).main()
 
     # 斷點續爬
-    Hjwzw(
-        novel_id=37018, 
-        novel_name="瘋狂升級系統", 
-        after_chapter_id=15775160,  # 包含此章節
-        is_continue=True
-    ).main()
+    # Hjwzw(
+    #     novel_id=35543, 
+    #     novel_name="重啟末世", 
+    #     after_chapter_id=11822905,  # 包含此章節
+    #     is_continue=True
+    # ).main()
